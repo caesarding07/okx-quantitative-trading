c6590c52b9ec3999b1d78a5d7fab16b8e8228c50
caesarding <caesarding07@gmail.com>
2022-02-13 22:22:03 +0800

    adjust

---
+++ src/api/authorization.sample.py
@@ -0,0 +1,10 @@
+# Get an Key and Secret
+# https://www.ouyicn.help/docs-v5/zh/#v5-api-key-creation
+
+# 请妥善保管好自己的API Key、Token.为了资金安全，请勿泄露!
+# Please take good care of your API Key and Token. For fund security, please do not disclose!  
+API_KEY = 'API KEY'
+SECRET_KEY = 'SECRET KEY'
+
+# https://oapi.dingtalk.com/robot/send?access_token=******
+DINGDING_TOKEN = 'DING DING TOKEN'
